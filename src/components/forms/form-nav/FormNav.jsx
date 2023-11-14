bd82c87e74ea844b94f3a0747e882fdfc89ca18c
Xcoder2023 <Xcoder2023@gmail.com>
2023-11-13 22:05:20 -0800

    styling form nav bar sticky

--- src/components/forms/form-nav/FormNav.jsx
+++ src/components/forms/form-nav/FormNav.jsx
@@ -7,10 +7,9 @@ import { Link } from "react-router-dom";
 const FormNav = () => {
   return (
     <>
-      <div className="form-nav">
-        <div className="mobile-form-nav">
-        <Link to='/' className='link-set'>
-          <div className="mobile-carriers">
+      <div className="mobile-form-nav">
+        <Link to="/" className="link-set">
+          <div className="mobile-carrier2">
             <div className="hambuer">
               <span className="bar"></span>
               <span className="bar"></span>
@@ -18,37 +17,37 @@ const FormNav = () => {
             </div>
             <p>Mich Homes</p>
           </div>
-          </Link>
-        </div>
-        <div className="nave-all">
-          <Link to="/">
-            <div className="brands-logo">
-              <img src={brands} alt="" />
-            </div>
-          </Link>
-          <div className="listings">
-            <ul>
-              <li>
-                <Link to="/latest" className="linksto">
-                  Latest Apartment Listings
-                </Link>
-              </li>
-              <li>Our Process</li>
-              <select className="mores">
-                <option>More</option>
-              </select>
-            </ul>
+        </Link>
+      </div>
+
+      <div className="nave-all">
+        <Link to="/">
+          <div className="brands-logo">
+            <img src={brands} alt="" />
           </div>
-          <div className="reaches-out">
-            <select className="locates">
-              <option>location</option>
+        </Link>
+        <div className="listings">
+          <ul>
+            <li>
+              <Link to="/latest" className="linksto">
+                Latest Apartment Listings
+              </Link>
+            </li>
+            <li>Our Process</li>
+            <select className="mores">
+              <option>More</option>
             </select>
-            <div className="teles">
-              <div className="teles-logo">
-                <img src={teles} alt="tele-icon" />
-              </div>
-              <p>Call / Text (234) 456 7890</p>
+          </ul>
+        </div>
+        <div className="reaches-out">
+          <select className="locates">
+            <option>location</option>
+          </select>
+          <div className="teles">
+            <div className="teles-logo">
+              <img src={teles} alt="tele-icon" />
             </div>
+            <p>Call / Text (234) 456 7890</p>
           </div>
         </div>
       </div>
